bc7ada8439bb44fe1e0b9ef884c1ccf1fc486fb5
hjl_3080server <cosmicdustycn@outlook.com>
2025-02-27 12:05:48 +0800

    [AUTO]  2025年 02月 27日 星期四 12:05:48 CST

---
+++ LeetCode/0231_power-of-two.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+    }
+};
+
+int main()
+{
+    int num = 16;
+    Solution solu;
+    std::cout << solu.isPowerOfTwo(num) << std::endl;
+
+    return 0;
+}
+
+/*
+
+*/
